b0c36098a6bff5524586fd41eb2cb919118b1f58
Jessin Abraham <jessingit5@gmail.com>
2026-08-07 13:04:47 -0400

    added preprocess to clean up data and pass that data for train test split

---
+++ src/preprocess.py
@@ -0,0 +1,38 @@
+import re
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+|www\S+", " ", text) # removes URLs
+    text = re.sub(r"[^a-z\s]", " ", text) # removed punctuation and numbers
+    text = re.sub(r"\s+", " ", text).strip()       # collapse whitespace
+    return text
+
+if __name__ == "__main__":
+    combined_dataset = script_dir.parent / "data" / "processed" / "kaggle_combined.csv"
+    train = script_dir.parent / "data" / "processed" / "train.csv"
+    test  = script_dir.parent / "data" / "processed" / "test.csv"
+
+    df = pd.read_csv(combined_dataset)
+
+    df["text"] = df["text"].apply(clean_text)
+    df = df[df["text"].str.strip() != ""]
+
+    train_df, test_df = train_test_split(
+        df, test_size=0.2, random_state=42, stratify=df["label"]
+    )
+    
+
+    train_df.to_csv(train, index=False)
+    test_df.to_csv(test, index=False)
+
+    print("Train shape:", train_df.shape)
+    print("Test shape:", test_df.shape)
+    print("\nTrain class balance:")
+    print(train_df["label"].value_counts(normalize=True))
+    print("\nTest class balance:")
+    print(test_df["label"].value_counts(normalize=True))
