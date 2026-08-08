3ee5da5c76812c1590ae8e9e960e740dce3d2cad
Jessin Abraham <jessingit5@gmail.com>
2026-08-08 11:59:24 -0400

    Updaing text_utils

--- src/text_utils.py
+++ src/text_utils.py
@@ -1,8 +1,11 @@
 import re
 
+ARTIFACT_WORDS = {"reuters"}
+
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"http\S+|www\S+", " ", text)
     text = re.sub(r"[^a-z\s]", " ", text)
     text = re.sub(r"\s+", " ", text).strip()
-    return text
+    words = [w for w in text.split() if w not in ARTIFACT_WORDS]
+    return " ".join(words)
